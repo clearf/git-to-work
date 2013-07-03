d53d4c7757f98ee4059b27b1e60ea7f53d1008cf
Clearfield Christopher <chris.clearfield@gmail.com>
2013-07-03 09:35:14 -0400

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 GitToWork::Application.routes.draw do
   resources :students
-
   resources :assignments
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'assignments#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
